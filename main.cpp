1f244795e2d0fe9504251199d3dec29d025e8955
brmcbrid <69523108+brmcbrid@users.noreply.github.com>
2025-11-04 17:30:44 -0500

    Initial Commit

--- main.cpp
+++ main.cpp
@@ -15,7 +15,7 @@ int main() {
     Circle c1, c2(5.25);
     c1.display();
     c2.display();
-    Cylinder cl1(3.5,8.65);
-    cl1.display();
+    Circle *cl1 = new Cylinder(3.5,8.65);
+    cl1->display();
     return 0;
 }
